5a77283056187ee9145d8f0777b38658a1ac7768
SARIAH TANNER <sariahtanner@gmail.com>
2024-05-21 19:49:01 -0600

    week 5 sql

--- databases/assignment2.sql
+++ databases/assignment2.sql
@@ -60,3 +60,26 @@ VALUES ('Custom'),
 	('SUV'),
 	('Truck'),
 	('Sedan');
+
+--Query One:
+INSERT INTO account(account_firstname, account_lastname, account_email, account_password) VALUES('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+--Query Two:
+UPDATE account SET account_type = 'Admin' WHERE account_lastname = 'Stark';
+
+--Query Three:
+DELETE FROM account WHERE account_firstname LIKE 'Tony';
+
+--Query Four:
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+--Query Five:
+SELECT inv_make, inv_model, classification_name
+FROM inventory i
+INNER JOIN classification c ON i.classification_id = c.classification_id;
+
+--Query Six:
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'), inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
